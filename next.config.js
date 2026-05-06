d03dc2093e05d9f21f54981719cc1b432dbb6241
mixrafynha <rafaelnonamer@gmail.com>
2026-05-06 21:00:53 +0200

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,16 +1,29 @@
-/** @type {import('next').NextConfig} */
-
 const nextConfig = {
-  reactStrictMode: true,
-  poweredByHeader: false,
-
   images: {
     remotePatterns: [
       {
         protocol: "https",
         hostname: "images.unsplash.com",
       },
+      {
+        protocol: "https",
+        hostname: "source.unsplash.com",
+      },
+
+      {
+        protocol: "https",
+        hostname: "*.r2.dev",
+      },
+
+      // localhost apenas dev
+      {
+        protocol: "http",
+        hostname: "localhost",
+      },
     ],
+
+    deviceSizes: [320, 420, 768, 1024, 1200, 1600],
+    imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
   },
 };
 
