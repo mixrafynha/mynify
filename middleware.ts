63ad81c6592fd08c3a7ee7e43ac8fb8c51a350d1
mixrafynha <rafaelnonamer@gmail.com>
2026-05-06 18:30:06 +0200

    Update middleware.ts

--- middleware.ts
+++ middleware.ts
@@ -209,7 +209,5 @@ export async function middleware(req: NextRequest) {
 }
 
 export const config = {
-  matcher: [
-    "/((?!_next/static|_next/image|favicon.ico|robots.txt|sitemap.xml|.*\\..*).*)",
-  ],
+  matcher: "/:path*",
 };
