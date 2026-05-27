d0a8b29c9cfa18818391d5a6b3e026f2067ac330
Rafael <rafaelnonamer@gmail.com>
2026-05-27 18:24:45 +0200

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -95,19 +95,12 @@ export const metadata = {
     },
   },
 
-  icons: {
-    icon: [
-      { url: "/favicon.ico" },
-      { url: "/favicon-64x64.png", sizes: "64x64", type: "image/png" },
-      { url: "/favicon-32x32.png", sizes: "32x32", type: "image/png" },
-      { url: "/favicon-16x16.png", sizes: "16x16", type: "image/png" },
-    ],
-
-    apple: [
-      {
-        url: "/apple-touch-icon.png",
-        sizes: "180x180",
-        type: "image/png",
+        icons: {
+        icon: [
+          { url: "/favicon.ico", sizes: "any", type: "image/x-icon" },
+        ],
+        shortcut: "/favicon.ico",
+        apple: "/apple-touch-icon.png",
       },
     ],
   },
